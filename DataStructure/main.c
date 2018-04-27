bf022a6a8796a14399a856bb9c97f723e81bd4ce
Jinchen Liu <jinchen@kth.se>
2018-04-28 01:44:31 +0200

    Initial Commit

---
+++ DataStructure/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  DataStructure
+//
+//  Created by 刘晋辰 on 2018-04-28.
+//  Copyright © 2018 刘晋辰. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
